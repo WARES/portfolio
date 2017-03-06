ea246eff959f56a10c4e7ac37667d14414a15203
Enrique Nares <e.nares.siurana@gmail.com>
2017-03-06 20:08:44 +0100

    Built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home', as: "pages"
+
+  get 'pages/about', as: "about"
+
+  get 'pages/contact', as: "contact"
+
+  resources :blogs
+  root "blogs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
