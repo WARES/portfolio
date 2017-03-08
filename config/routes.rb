d27d824c43c5b3d45f1c98e6fba3c142c8735dae
Enrique Nares <e.nares.siurana@gmail.com>
2017-03-08 17:54:02 +0100

    Add custom routes for about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :works
-  get 'pages/home', as: "pages"
-
-  get 'pages/about', as: "about"
-
-  get 'pages/contact', as: "contact"
+  
+  #get 'pages/about', as: "about"
+  get 'about', to: 'pages#about'
+  #get 'pages/contact', as: "contact"
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
-  root "blogs#index"
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
